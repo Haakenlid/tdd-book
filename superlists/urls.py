d4ebbd55cf5e77be4eafc81cd4f65d3dd06992a8
Håken Lid <haakenlid@gmail.com>
2014-07-24 00:22:56 +0200

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+# from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
     # url(r'^admin/', include(admin.site.urls)),
 )
