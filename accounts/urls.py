2e603d27612dcd5187ef89ab16c279ccecd5446b
Håken Lid <haakenlid@gmail.com>
2014-11-14 19:12:34 +0100

    Functional tests are passing for both login and logout.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, patterns
 from .views import persona_login
+from django.contrib.auth.views import logout
 
 urlpatterns = patterns(
     '',
     url(r'^login$', persona_login, name='persona_login',),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout',),
 )
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -27,23 +27,54 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees it's a real sessin login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Terrified of theis new feature, she reflexively clicks "logout".
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The "logged out" status also persists after a refresh.
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
+
+    def wait_to_be_logged_in(self):
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
+
     def switch_to_new_window(self, text_in_title):
         retries = 20
         while retries > 0:
+            page_titles = []
             for handle in self.browser.window_handles:
                 self.browser.switch_to_window(handle)
                 if text_in_title in self.browser.title:
                     return
-            print(self.browser.title)
+                page_titles.append(self.browser.title)
             retries -= 1
             time.sleep(0.5)
-        self.fail('could not find window "{}"'.format(text_in_title))
+        self.fail(
+            'could not find window "{}". Pages are \n{}'.format(
+                text_in_title,
+                page_titles,
+            )
+        )
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=10).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'could not find element with id "{}". Page content was \n{}'.format(
+                element_id,
+                self.browser.find_element_by_tag_name('body').text,
+            ),
         )
