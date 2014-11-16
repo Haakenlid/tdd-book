0400f1cd32ef2fb94a64fd587e0c2e87cd63bf6b
Håken Lid <haakenlid@gmail.com>
2014-11-16 23:44:19 +0100

    List owner is implemented. FTs are passing

--- lists/models.py
+++ lists/models.py
@@ -6,6 +6,13 @@ from django.conf import settings
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
+    @property
+    def name(self):
+        if self.item_set:
+            return self.item_set.first().text
+        else:
+            return 'empty list {pk}'.format(self.pk)
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
